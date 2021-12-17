945e9c33269614a1a7df6c2c38db3c6e4e4a8594
Vitor <vfarias@bionexo.com>
2021-12-17 00:24:24 -0400

    Add docker-compose with Redis

---
+++ BasicRedisChat/Configs/RedisSettings.cs
@@ -0,0 +1,9 @@
+namespace BasicRedisChat.Configs
+{
+    public class RedisSettings
+    {
+        public string Url { get; set; }
+        public string Port { get; set; }
+        public string Password { get; set; }
+    }
+}
